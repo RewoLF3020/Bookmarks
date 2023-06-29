7676a3af7e82c01d5a007ec7c4feee66a091993e
RewoLF3020 <zolotovvladislav305@gmail.com>
2023-06-29 18:52:49 +0300

    user's profiles and registration added

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm,\
+                   UserEditForm, ProfileEditForm 
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+from django.contrib import messages
 
 
 
@@ -32,3 +35,48 @@ def dashboard(request):
     return render(request,
                   'account/dashboard.html',
                   {'section': 'dashboard'})
+    
+
+def register(requset):
+    if requset.method == 'POST':
+        user_form = UserRegistrationForm(requset.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(requset,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(requset,
+                  'account/register.html',
+                  {'user_form': user_form})
+    
+    
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile,
+                                    data=request.POST,
+                                    files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated '\
+                                      'Successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+                                    instance=request.user.profile)
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
