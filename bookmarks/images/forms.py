e9bb1302cf3e6547c1be0d415a4490d6c2953589
RewoLF3020 <zolotovvladislav305@gmail.com>
2023-07-03 18:38:46 +0300

    infinity scroll and other ajax added

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -16,12 +16,12 @@ class ImageCreateForm(forms.ModelForm):
     def clean_url(self):
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg', 'png']
-        extensions = url.rsplit('.', 1)[1].lower()
-        if extensions not in valid_extensions:
-            raise forms.ValidationError('The given URL does not'\
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not ' \
                                         'match valid image extensions.')
         return url
-    
+
     def save(self, force_insert=False,
                    force_update=False,
                    commit=True):
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
-from django.shortcuts import get_object_or_404
 from .models import Image
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, \
+                                  PageNotAnInteger
 
 
 @login_required
@@ -38,3 +41,52 @@ def image_detail(request, id, slug):
                   'images/image/detail.html',
                   {'section': 'images',
                    'image': image}) 
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'OK'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # Если страница не является целым числом,
+        # то доставить первую страницу
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            # Если AJAX-запрос и страница вне диапазона,
+            # то вернуть пустую страницу
+            return HttpResponse('')
+        # Если страница вне диапазона,
+        # то вернуть последнюю страницу результатов
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request,
+                      'images/image/list_images.html',
+                      {'section': 'images',
+                       'images': images})
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images',
+                   'images': images})
